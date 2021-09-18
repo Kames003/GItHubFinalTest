3eb05bf76ee5c5619eab8f5c81b8a1b18df952bd
Kames003 <tomasmucha1314@gmail.com>
2021-09-18 20:56:48 +0200

    Initial Commit

---
+++ GItHubFinalTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GItHubFinalTest
+//
+//  Created by Tomáš Mucha on 18/09/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
